d280492ed760322ede714c243125e0f4b2ba10cf
AngelSalgadoM <angel.salgado@alumnos.uv.cl>
2023-11-27 02:59:20 -0300

    Arreglo de Routers

--- routes/routerDireccion.js
+++ routes/routerDireccion.js
@@ -5,7 +5,7 @@ const routerDireccion = Router();
 import sqlDireccion  from "../controllers/direccion/sql/direccionController.js";
 
 routerDireccion.get('/allDirecciones/:typeBd', sqlDireccion.listarDirecciones);
-routerDireccion.get('/:id/:typeBd', sqlDireccion.listarById);
-routerDireccion.put('/:id/:typeBd', sqlDireccion.cambiarDatos);
+routerDireccion.get('/obtenerPorId/:id/:typeBd', sqlDireccion.listarById);
+routerDireccion.put('/updateDireccion/:id/:typeBd', sqlDireccion.cambiarDatos);
 
 export default routerDireccion;
