a889ea59e57d7e8a0f9f39ffc5d8d2b43d4553fa
AngelSalgadoM <angel.salgado@alumnos.uv.cl>
2023-11-27 02:51:08 -0300

    Metodo direccion, cambiarDatos

--- routes/routerDireccion.js
+++ routes/routerDireccion.js
@@ -6,5 +6,6 @@ import sqlDireccion  from "../controllers/direccion/sql/direccionController.js";
 
 routerDireccion.get('/allDirecciones/:typeBd', sqlDireccion.listarDirecciones);
 routerDireccion.get('/:id/:typeBd', sqlDireccion.listarById);
+routerDireccion.put('/:id/:typeBd', sqlDireccion.cambiarDatos);
 
 export default routerDireccion;
